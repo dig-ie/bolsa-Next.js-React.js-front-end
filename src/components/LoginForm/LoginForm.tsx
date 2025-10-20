e2911a89cf3641aa592183c2d8fe2c9ee1ea96d0
Pablo JDev <pablodosantos2007@gmail.com>
2025-10-20 19:51:44 -0300

    refactor: Replaces hardcoded values with design variables and changes placeholder size and fonts

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -7,15 +7,15 @@ const LoginForm: React.FC<LoginFormProps> = ({ onlyEmail = false }) => {
       <div className="flex flex-col">
         <label
           htmlFor="email"
-          className="font-medium mb-2 text-base sm:text-lg md:text-xl lg:text-xl text-left lg:mb-1"
+          className="font-bold text-2xl text-textClaro sm:text-lg md:text-xl lg:text-xl text-left lg:mb-1"
         >
-          Email
+          Email:
         </label>
         <input
           id="email"
           type="email"
-          placeholder="Digite seu email"
-          className="px-4 py-2 rounded-2xl border border-textClaro bg-backgroundClaro text-sm mt-2 w-[320px] lg:w-[360px] max-w-md lg:mt-1 lg:rounded-[9px]"
+          placeholder="usuário123@gmail.com"
+          className="px-4 py-2 rounded-2xl font-normal bg-secondaryText text-sm mt-2 w-[320px] lg:w-[415px] max-w-md lg:mt-1 lg:rounded-[9px] lg:bg-backgroundClaro shadow-md/40"
           autoComplete="email"
         />
       </div>
@@ -23,15 +23,15 @@ const LoginForm: React.FC<LoginFormProps> = ({ onlyEmail = false }) => {
       <div className="flex flex-col">
         <label
           htmlFor="senha"
-          className="font-medium mb-2 text-base sm:text-lg md:text-xl lg:text-xl text-left lg:mb-1"
+          className="font-bold text-2xl text-textClaro sm:text-lg md:text-xl lg:text-xl text-left lg:mb-1"
         >
-          Senha
+          Senha:
         </label>
         <input
           id="senha"
           type="password"
-          placeholder="Digite sua senha"
-          className="px-4 py-2 rounded-2xl border border-textClaro bg-backgroundClaro text-sm mt-2 w-[320px] lg:w-[360px] max-w-md lg:mt-1 lg:rounded-[9px]"
+          placeholder="********"
+          className="px-4 py-2 rounded-2xl font-normal bg-secondaryText text-sm mt-2 w-[320px] lg:w-[415px] max-w-md lg:mt-1 lg:rounded-[9px] lg:bg-backgroundClaro shadow-md/40"
           autoComplete="current-password"
         />
       </div>
