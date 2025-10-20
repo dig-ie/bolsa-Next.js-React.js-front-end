99c6b06b2ae886dd722002a5178e388d11f823f3
Pablo JDev <pablodosantos2007@gmail.com>
2025-10-20 19:59:50 -0300

    style: adjusted style, size and spacing for mobile and tables screens

--- src/app/ForgetPassword/page.tsx
+++ src/app/ForgetPassword/page.tsx
@@ -7,10 +7,12 @@ export default function ForgetPassword() {
   return (
     <div className="bg-primaryGreen">
       <BackButton href="./LoginPage" />
-      <div className="flex flex-col items-center justify-center   relative  h-40">
-        <Text h1="Recuperar Senha" h2="" p="" />
+      <div className="flex flex-col items-center justify-center relative h-30 mb-10">
+        <Text className="lg:hidden" h1="Recuperar Senha" h2="" p="" />
       </div>
-      <div className="bg-white rounded-t-4xl py-25 flex flex-col gap-5">
+
+      <div className="bg-white w-full min-h-[80vh] rounded-t-4xl flex flex-col items-center justify-star pt-6">
+        <div className="flex flex-col gap-2 mt-[3vh]">
         <Text
           h1=""
           h2="Esqueceu a Senha?"
@@ -19,10 +21,11 @@ export default function ForgetPassword() {
         <div className="flex flex-col items-center justify-center">
           <LoginForm onlyEmail={true}/>
         </div>
-        <div className="flex flex-col items-center justify-center">
-          <Submit title="Entrar" type={"submit"} href="" />
-        </div>
+       <div className="flex flex-col items-center justify-center">
+          <Submit title="Entrar" type="submit" href="" className="w-[320px] h-[58px] border rounded-xl bg-secondaryColor mt-6 cursor-pointer"/>
+          </div>
       </div>
+    </div>
     </div>
   );
 }
