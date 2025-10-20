b3d22133a591bf0c582e44724131f44175a12054
Pablo JDev <pablodosantos2007@gmail.com>
2025-10-19 23:52:53 -0300

    style: adjusts the style of the Submit component with a new class and spacing

--- src/components/Submit/Submit.tsx
+++ src/components/Submit/Submit.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { SubmitProps } from "./SubmitProps";
 
-const Submit: React.FC <SubmitProps> =
-({title, href, className}) => {
-    return (
-        <a href={href} className="h-px">
-        <button className={className}>
-            <h1 className="font-bold text-white">
-            {title}
-            </h1>
-        </button>
-        </a>
-    )
-}
+const Submit: React.FC<SubmitProps> = ({ title, href, className }) => {
+  return (
+    <a href={href} className="h-px">
+      <button className={className}>
+        <h1 className="font-bold text-backgroundClaro text-[20px]">{title}</h1>
+      </button>
+    </a>
+  );
+};
 
 export default Submit;
