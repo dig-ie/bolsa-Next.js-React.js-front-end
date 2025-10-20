9939f9029dcf2c4126b274a4a6744e4312e5388e
Pablo JDev <pablodosantos2007@gmail.com>
2025-10-20 19:57:04 -0300

    refactor: optimizes LoginForm size and spacing

--- src/components/LoginForm/LoginContent/index.tsx
+++ src/components/LoginForm/LoginContent/index.tsx
@@ -5,6 +5,7 @@ import LoginForm from "../../../components/LoginForm/LoginForm";
 import Text from "../../../components/Text/Text";
 import Link from "../../../components/CustomLink/CustomLink";
 import Submit from "../../../components/Submit/Submit";
+import BackButton from "../../../components/BackButton/BackButton";
 
 interface LoginContentProps {
   facebookTitle: string;
@@ -14,12 +15,13 @@ interface LoginContentProps {
 export default function LoginContent({ facebookTitle, googleTitle }: LoginContentProps) {
   return (
     <div className="bg-primaryGreen">
-
-      <div className="flex flex-col items-center justify-center relative h-40">
+      <BackButton href="./mainPage" />
+      <div className="flex flex-col items-center justify-center relative h-30 mb-10">
         <Text className="lg:hidden" h1="Faça seu Login." h2="" p="" />
       </div>
 
-      <div className="bg-white rounded-t-4xl py-25 flex flex-col gap-5 items-center justify-center">
+      <div className="bg-white w-full min-h-[80vh] rounded-t-4xl flex flex-col items-center justify-star pt-6">
+        <div className="flex flex-col gap-2 mt-[3vh]">
         <LoginForm />
         <Link text="Esqueceu a senha?" />
 
@@ -33,11 +35,12 @@ export default function LoginContent({ facebookTitle, googleTitle }: LoginConten
             title={googleTitle}
             href="https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fmyaccount.google.com%2F%3Futm_source%3Dsign_in_no_continue%26pli%3D1&ec=GAlAwAE&hl=pt_BR&service=accountsettings&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S-687016799%3A1747074790770482"
           />
-        </div>
 
-        <div className="flex flex-col items-center justify-center">
-          <Submit title="Entrar" type="submit" href="" className="w-[320px] h-[58px] border rounded-xl bg-secondaryColor mt-6 cursor-pointer"/>
         </div>
+        <div>
+          <Submit title="Entrar" type="submit" href="" className="w-[320px] h-[58px] border rounded-xl bg-secondaryColor mt-6 cursor-pointer"/>
+          </div>
+          </div>
       </div>
     </div>
   );
