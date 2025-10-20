1f13c0619255cc01bfdae4c0be02131aca566299
Pablo JDev <pablodosantos2007@gmail.com>
2025-10-20 19:53:19 -0300

    style: remove edge of the BackButton component

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -7,7 +7,7 @@ import {FaArrowLeft} from "react-icons/fa"
 const BackButton: React.FC<BackButtonProps> = ({ href }) => {
   return (
     <Link href={href}>
-      <button className="w-[51px] h-[46px] border rounded-xl bg-white cursor-pointer mt-5 ml-2.5">
+      <button className="w-[51px] h-[46px] rounded-xl bg-white cursor-pointer mt-5 ml-2.5">
         {<FaArrowLeft className="w-[45px] text-[22px]" />}
       </button>
     </Link>
