38d52ddea800cd32718b9f075fc3d3d15033db10
Pablo JDev <pablodosantos2007@gmail.com>
2025-10-24 16:23:42 -0300

    feat: Refactor the Separator component, using width 100% and text as prop

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -1,12 +1,12 @@
-import { SeparatorProps } from './SeparaotProps';
-import React from 'react';
+import { SeparatorProps } from "./SeparatorProps";
+import React from "react";
 
-export const Separator: React.FC<SeparatorProps> = () => {
-    return (
-        <div className="flex items-center my-4 w-[415px] h-px mt-8"> 
-            <div className="flex-grow h-px bg-gray-400"></div>
-            <span className="mx-4 shrink-0 text-sm text-gray-400">ou</span>
-            <div className="flex-grow h-px bg-gray-400"></div>
-        </div>
-    );
+export const Separator: React.FC<SeparatorProps> = ({ text = "ou" }) => {
+  return (
+    <div className="flex items-center my-4 w-[100%] h-px mt-8">
+      <div className="flex-grow h-px bg-gray-400"></div>
+      <span className="mx-4 shrink-0 text-sm text-gray-400">{text}</span>
+      <div className="flex-grow h-px bg-gray-400"></div>
+    </div>
+  );
 };
