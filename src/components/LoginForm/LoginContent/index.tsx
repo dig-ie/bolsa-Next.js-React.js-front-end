e5c63e7b54d2c792cfa9f0ab6b4a58d14d93aa16
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-11-01 08:28:49 -0300

    refactor: Update LoginContent to use ContentCard for improved layout and add responsive design handling

--- src/components/LoginForm/LoginContent/index.tsx
+++ src/components/LoginForm/LoginContent/index.tsx
@@ -5,18 +5,20 @@ import LoginForm from "../../../components/LoginForm/LoginForm";
 import AuthLink from "../../AuthLink/AuthLink";
 import Submit from "../../SubmitButton/SubmitButton";
 import BackButton from "../../../components/BackButton/BackButton";
+import ContentCard from "../ContentCard/ContentCard";
+import useResponsive from "@/hooks/useResponsive";
 
 interface LoginContentProps {
-  authButtonPrimaryText: string;
-  authButtonPrimaryHref: string;
-  authButtonSecondaryText: string;
-  authButtonSecondaryHref: string;
+  authButtonPrimaryText?: string;
+  authButtonPrimaryHref?: string;
+  authButtonSecondaryText?: string;
+  authButtonSecondaryHref?: string;
   submitTitle?: string;
   backButtonHref?: string;
   forgotPasswordText?: string;
   forgotPasswordHref?: string;
-  registerText: string;
-  registerHref: string;
+  registerText?: string;
+  registerHref?: string;
 }
 
 export default function LoginContent({
@@ -29,35 +31,39 @@ export default function LoginContent({
   forgotPasswordText = "Esqueceu a senha?",
   forgotPasswordHref = "/ForgetPassword",
 }: LoginContentProps) {
-  return (
-    <div className="bg-primaryGreen">
+  const { isLg } = useResponsive();
+
+  return isLg ? (
+    <LoginForm />
+  ) : (
+    <div className="bg-primaryGreen relative min-h-screen">
       <BackButton href={backButtonHref} />
-      <div className="flex flex-col items-center justify-center relative h-30 mb-10">
+      <div className="flex flex-col items-center justify-center relative h-30 mb-10 pt-20">
         <h1 className="lg:hidden text-4xl font-bold">Faça seu Login</h1>
       </div>
 
-      <div className="bg-white w-full min-h-[80vh] rounded-t-4xl flex flex-col items-center justify-star pt-6">
-        <div className="flex flex-col gap-2 mt-[3vh]">
-          <LoginForm />
-          <AuthLink href={forgotPasswordHref} text={forgotPasswordText} />
-
-          <div className="flex flex-row items-center justify-center gap-4 mt-4 md:flex md:items-center md:justify-center md:gap-4 md:mt-4">
-            <SecondaryButton
-              imageSrc="/images/facebook_icon.png"
-              title={authButtonPrimaryText} destination={authButtonPrimaryHref}            />
-            <SecondaryButton
-              imageSrc="/images/google_icon.png"
-              title={authButtonSecondaryText} destination={authButtonSecondaryHref}            />
-          </div>
-          <div>
-            <Submit
-              title={submitTitle}
-              type="submit"
-              className="w-[320px] h-[58px] border rounded-xl bg-secondaryColor mt-6 cursor-pointer"
-            />
-          </div>
+      <ContentCard>
+        <LoginForm />
+        <AuthLink href={forgotPasswordHref} text={forgotPasswordText} />
+        <div className="flex flex-row items-center justify-center gap-4 mt-4 md:flex  md:gap-4 md:mt-4">
+          <SecondaryButton
+            imageSrc="/images/facebook_icon.png"
+            title={authButtonPrimaryText}
+            destination={authButtonPrimaryHref}
+          />
+          <SecondaryButton
+            imageSrc="/images/google_icon.png"
+            title={authButtonSecondaryText}
+            destination={authButtonSecondaryHref}
+          />
         </div>
-      </div>
+
+        <Submit
+          title={submitTitle}
+          type="submit"
+          className="w-[320px] h-[58px] border rounded-xl bg-secondaryColor cursor-pointer"
+        />
+      </ContentCard>
     </div>
   );
 }
