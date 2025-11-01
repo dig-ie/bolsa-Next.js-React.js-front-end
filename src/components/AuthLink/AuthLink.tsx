c3630f618895fc2c496b813f7c2ba192c7ad7cba
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-11-01 08:28:22 -0300

    fix: Adjust margin for responsive design in AuthLink component

--- src/components/AuthLink/AuthLink.tsx
+++ src/components/AuthLink/AuthLink.tsx
@@ -5,7 +5,7 @@ import { AuthLinkProps } from "./AuthLinkProps";
 const CustomLink: React.FC<AuthLinkProps> = ({ text, href }) => {
   return (
     <Link
-      className="text-linkBlue font-bold text-[20px] max-w-md lg:text-[13px] md:text-[18px] items-center justify-center lg:underline mr-[140px] md:mr-[150px] lg:mr-0 "
+      className="text-linkBlue font-bold text-[20px] max-w-md lg:text-[13px] md:text-[18px] items-center justify-center lg:underline md:mr-[150px] lg:mr-0 "
       href={href}
     >
       {text}
