958eacb80beb6a223cdae3060deebe176bda2b5e
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-11-09 06:02:17 -0300

    fix: correct syntax and formatting in BottomNavBar component

--- src/components/Layout/BottomNavBar/index.tsx
+++ src/components/Layout/BottomNavBar/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import { IoIosStats } from "react-icons/io";
 import { FaWallet, FaTrophy } from "react-icons/fa";
@@ -8,35 +8,35 @@ import ButtonSmall from "./components/buttonSmall";
 import { usePathname } from "next/navigation";
 
 const navItems = [
-    { icon: IoIosStats, alt: "Stats", href: "#" },
-    { icon: FaWallet, alt: "Wallet", href: "#" },
-    { icon: BiHome, alt: "Home", href: "/mainDashboard" },
-    { icon: FaTrophy, alt: "Trophy", href: "#" },
-    { icon: CgProfile, alt: "Profile", href: "#" },
+  { icon: IoIosStats, alt: "Stats", href: "#" },
+  { icon: FaWallet, alt: "Wallet", href: "#" },
+  { icon: BiHome, alt: "Home", href: "/mainDashboard" },
+  { icon: FaTrophy, alt: "Trophy", href: "#" },
+  { icon: CgProfile, alt: "Profile", href: "#" },
 ];
 
 const BottomNav: React.FC = () => {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    return (
-        <nav
-            aria-label="Navegação principal"
-            className="bg-[#2ECC71] w-full p-2 fixed bottom-0 left-0 right-0 border-t border-gray-200"
-        >
-            <ul className="flex flex-row items-center justify-around">
-                {navItems.map((item, index) => (
-                    <li key={index}>
-                        <ButtonSmall
-                            href={item.href}
-                            icon={item.icon}
-                            alt={item.alt}
-                            isActive={pathname === item.href}
-                        />
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
+  return (
+    <nav
+      aria-label="Navegação principal"
+      className="bg-secondaryGreen w-full p-2 fixed bottom-0 left-0 border-t border-none rounded-t-2xl"
+    >
+      <ul className="flex flex-row items-center justify-around">
+        {navItems.map((item, index) => (
+          <li key={index}>
+            <ButtonSmall
+              href={item.href}
+              icon={item.icon}
+              alt={item.alt}
+              isActive={pathname === item.href}
+            />
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default BottomNav;
