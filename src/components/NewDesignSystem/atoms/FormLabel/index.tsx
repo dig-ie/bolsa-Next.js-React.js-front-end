79d8a8feacd9c81608b000ba0946a0f704ed2acf
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-11-09 05:29:37 -0300

    feat: add FormLabel component with props for text and required indicator

---
+++ src/components/NewDesignSystem/atoms/FormLabel/index.tsx
@@ -0,0 +1,13 @@
+import { FormLabelProps } from "./props";
+
+export function FormLabel({
+  text = "default",
+  required = true,
+}: FormLabelProps) {
+  return (
+    <label className="text-sm font-medium text-darkText self-start">
+      {text + ":"}
+      {required && <span className="text-linkBlue">*</span>}
+    </label>
+  );
+}
