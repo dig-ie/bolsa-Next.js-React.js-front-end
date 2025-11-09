58699d211b4aaf725239314905ed0e9d3d34f6c3
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-11-09 11:12:30 -0300

    feat(PrimaryButton): add atom primary button component with variants

---
+++ src/components/NewDesignSystem/atoms/PrimaryButton/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { tv } from "tailwind-variants";
+
+const primaryButton = tv({
+  base: `font-bold text-xl rounded-lg`,
+  variants: {
+    size: { md: "h-12 w-36", lg: "h-13 w-68" },
+    colors: {
+      default: "bg-primaryGreen text-white",
+      secondary: "bg-white text-darkText",
+    },
+  },
+  defaultVariants: { size: "md" },
+});
+
+type PrimaryButtonVariants = Parameters<typeof primaryButton>[0];
+
+export type PrimaryButtonProps = React.ComponentPropsWithoutRef<"button"> &
+  PrimaryButtonVariants;
+
+export function PrimaryButton({
+  className,
+  size = "md",
+  colors = "default",
+  ...props
+}: PrimaryButtonProps) {
+  return (
+    <button className={primaryButton({ size, colors, className })} {...props} />
+  );
+}
