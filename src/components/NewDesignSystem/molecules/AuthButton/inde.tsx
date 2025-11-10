5f374f190d0738e73b6e4d9668ebf65303fad2e2
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-11-10 15:28:33 -0300

    chore: update TODO

--- src/components/NewDesignSystem/molecules/AuthButton/inde.tsx
+++ src/components/NewDesignSystem/molecules/AuthButton/inde.tsx
@@ -2,7 +2,7 @@
 
 type AuthButtonProps = React.ComponentPropsWithoutRef<"button">;
 // Este é o botão de autenticação.
-// TODO: Implementar lógica de icones e estilos. Para lógica de ícones, seguir o exemplo do FormInputWithIcon, porém utilizando imagens (svg em public) apropriadas para autenticação (Google, Facebook, etc).
+// TODO: Thiago -> Implementar lógica de icones e estilos. Para lógica de ícones, seguir o exemplo do FormInputWithIcon, porém utilizando imagens (svg em public) apropriadas para autenticação (Google, Facebook, etc). Caso tenha dúvida de component de molecula, olhar outros exemplos na pasta molecules.
 export function AuthButton({ className, children, ...props }: AuthButtonProps) {
   return (
     <div>
