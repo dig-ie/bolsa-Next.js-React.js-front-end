a0b306a27a45bba394e5adc50fdeec52c8b45469
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-11-16 13:12:39 -0300

    feat: add login route and separate test route

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,36 +1,15 @@
-import { CardSurface } from "@/components/NewDesignSystem/atoms/CardSurface";
-import { FormInput } from "@/components/NewDesignSystem/atoms/FormInput";
-import { FormLabel } from "@/components/NewDesignSystem/atoms/FormLabel";
-import { PrimaryButton } from "@/components/NewDesignSystem/atoms/PrimaryButton";
-import { AuthButton } from "@/components/NewDesignSystem/molecules/AuthButton/inde";
-import { FormInputPassword } from "@/components/NewDesignSystem/molecules/FormInputPassword";
-import { FormInputWithIcon } from "@/components/NewDesignSystem/molecules/FormInputWithIcon";
-import { PasswordRecoveryCard } from "@/components/NewDesignSystem/organisms/PasswordRecoveryCard";
+"use client";
+import { LoginCard } from "@/components/NewDesignSystem/organisms/LoginCard";
+import { AuthPageTemplate } from "@/components/NewDesignSystem/templates/AuthPageTemplate";
 
-export default function LoginPage() {
+export default function Login() {
   return (
-    <>
-      <CardSurface className="flex flex-col gap-2 my-5 pb-8">
-        <h2>Componentização</h2>
-        <FormLabel text="Email" required={true} />
-        <FormInput intent="default" />
-        <div className="bg-gray-700 p-1 flex flex-col gap-1">
-          <PrimaryButton>Entrar</PrimaryButton>
-          <PrimaryButton colors="secondary">Cadastre-se</PrimaryButton>
-          <PrimaryButton size="lg">Entrar</PrimaryButton>
-          <PrimaryButton size="lg" colors="secondary">
-            Cadastre-se
-          </PrimaryButton>
-        </div>
-        <AuthButton>TESTE</AuthButton>
-        <FormInputWithIcon />
-        <FormInputPassword />
-        <FormInputPassword />
-      </CardSurface>
-      <PasswordRecoveryCard />
-      <br />
-      <br />
-      <br />
-    </>
+    // <div className="lg:bg-white w-screen h-screen">
+    //   Login
+    //   <LoginCard />
+    // </div>
+    <AuthPageTemplate title="Login">
+      <LoginCard className="h-full w-full" />
+    </AuthPageTemplate>
   );
 }
