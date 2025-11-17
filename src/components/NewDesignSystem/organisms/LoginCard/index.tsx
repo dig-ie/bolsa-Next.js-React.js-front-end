8a20fd478ba7876b85c69a2dfe5bc67f1a7c5f67
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-11-17 18:00:22 -0300

    feat(LoginCard/organism): add login card organism and make it controll auth/login api call request

--- src/components/NewDesignSystem/organisms/LoginCard/index.tsx
+++ src/components/NewDesignSystem/organisms/LoginCard/index.tsx
@@ -1,16 +1,45 @@
 "use-client";
+import { useState } from "react";
 import { CardSurface } from "../../atoms/CardSurface";
 import { LoginForm } from "../../molecules/LoginForm";
 
 type LoginCardProps = React.ComponentPropsWithoutRef<"div">;
+
 export function LoginCard({ className, ...props }: LoginCardProps) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  async function handleLogin(email: string, password: string) {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const res = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        credentials: "include",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!res.ok) {
+        const data = await res.json().catch(() => ({}));
+      }
+
+      const data = await res.json();
+      console.log(data);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Erro inesperado");
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
     <CardSurface className={`mx-1.5 h-[70vh] pt-14 pb-4 ${className}`}>
-      <LoginForm
-        onSubmit={function (email: string, password: string): void {
-          throw new Error("Function not implemented.");
-        }}
-      />
+      <LoginForm onSubmit={handleLogin} />
     </CardSurface>
   );
 }
