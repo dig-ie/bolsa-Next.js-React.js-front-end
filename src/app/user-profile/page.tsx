1fc8777d01114d82be1be5062e1d1c1e053f0575
Pablo JDev <pablodosantos2007@gmail.com>
2025-11-19 16:27:34 -0300

    feat(pages): Add user profile page

---
+++ src/app/user-profile/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import EditProfileTemplate from "@/components/NewDesignSystem/templates/EditProfileTemplate";
+export default function UserProfilePage() {
+    return (
+        <EditProfileTemplate />
+    );
+};
